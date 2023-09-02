5fce3b47de97fa63fcfb8635c5a8b47a6a52fb74
json <basartejason@gmail.com>
2023-09-02 08:04:19 +0800

    added appsettings

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -2,9 +2,14 @@
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "json@thepet.com";
-        private string _mailFrom = "noreply@thepet.com";
+        private readonly string _mailTo = string.Empty;
+        private readonly string _mailFrom = string.Empty;
 
+        public CloudMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
         public void Send(string subject, string message)
         {
             Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}, " + $"with {nameof(CloudMailService)}.");
